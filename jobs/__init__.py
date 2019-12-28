ffe8215b1dc787ffb83ca1df43dc77a61b51083f
dzkb <dzakub@gmail.com>
2019-12-28 14:54:27 +0100

    Implement base morele.net job

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+from pytz import utc
 
 from jobs.base import JobDefinition
 from jobs.morele import run as morele
@@ -8,5 +12,11 @@ all_jobs = [
     JobDefinition(
         id="xkom", function=xkom, trigger=CronTrigger(hour="10,22", second="10")
     ),
-    JobDefinition(id="morele", function=morele, trigger=None),
+    JobDefinition(
+        id="morele",
+        function=morele,
+        trigger=IntervalTrigger(
+            start_date=datetime(2019, 12, 27, 14, 0, tzinfo=utc), days=3, timezone=utc
+        ),
+    ),
 ]
--- jobs/morele.py
+++ jobs/morele.py
@@ -1,2 +1,18 @@
+import requests
+
+import settings
+from formatters import format_offer_discord
+from hooks import discord_hook
+from jobs.base import Offer
+
+
+def _parse_morele(morele_site):
+    return Offer(title="", description="", offer_url="", image_url="")
+
+
 def run():
-    pass
+    morele_site = requests.get(settings.MORELE_DATA_URL)
+    offer = _parse_morele(morele_site.text)
+
+    payload = format_offer_discord(offer)
+    discord_hook(settings.MORELE_DISCORD_HOOK_URL, payload)
