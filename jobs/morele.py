50c0c7581180fd4e994258d440fbcca3ed5c1062
dzkb <dzakub@gmail.com>
2019-12-30 09:55:24 +0100

    Implement morele.net parser

--- jobs/morele.py
+++ jobs/morele.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 import settings
 from formatters import format_offer_discord
@@ -7,7 +8,22 @@ from jobs.base import Offer
 
 
 def _parse_morele(morele_site):
-    return Offer(title="", description="", offer_url="", image_url="")
+    morele_soup = BeautifulSoup(morele_site, "html.parser")
+
+    product_link = morele_soup.find("div", {"class": "promo-box-name"}).a
+    title = product_link.text
+    offer_url = product_link["href"]
+    image_url = morele_soup.find("a", {"class": "prom-box-image"}).img["src"]
+
+    old_price = morele_soup.find("div", {"class": "promo-box-old-price"}).text
+    new_price = morele_soup.find("div", {"class": "promo-box-new-price"}).text
+    coupon = morele_soup.find("div", {"class": "promo-box-code-value"}).text
+
+    description = f"{old_price} → {new_price} (kod: {coupon})"
+
+    return Offer(
+        title=title, description=description, offer_url=offer_url, image_url=image_url
+    )
 
 
 def run():
